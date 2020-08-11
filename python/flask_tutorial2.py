eae8bd6f3a91b4f3f668783561f1af045508ce82
MyeongwonKim <samuelmwkim@gmail.com>
2020-08-11 19:44:51 +0900

    python flask

---
+++ python/flask_tutorial2.py
@@ -0,0 +1,42 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True             #make the session here permanent
+        user = request.form["nm"]            #get the data from the input text
+        session["user"] = user               #put the data into session
+        flash("Login Successful!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already Logged In!")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out!", "info")
+    session.pop("user", None)                 #removing data from the session
+    return redirect(url_for("login"))
+
+if __name__=="__main__":
+    app.run(debug=True)
