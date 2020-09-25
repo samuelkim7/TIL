e51cb5653c34f32e6df45e8cf9a542b8e058b441
MyeongwonKim <samuelmwkim@gmail.com>
2020-09-25 10:13:15 +0900

    spring-boot board

---
+++ spring/spring-boot_board/src/main/java/board/controller/BoardController.java
@@ -0,0 +1,39 @@
+package board.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import board.dto.BoardDto;
+import board.service.BoardService;
+
+@Controller
+public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping("/board/openBoardList.do")
+	public ModelAndView openBoardList() throws Exception{
+		ModelAndView mv = new ModelAndView("/board/boardList");
+		
+		List<BoardDto> list = boardService.selectBoardList();
+		mv.addObject("list", list);
+		
+		return mv;
+	}
+	
+	@RequestMapping("/board/openBoardWrite.do")
+	public String openBoardWrite() throws Exception{
+		return "/board/boardWrite";
+	}
+	
+	@RequestMapping("/board/insertBoard.do")
+	public String insertBaord(BoardDto board) throws Exception{
+		boardService.insertBoard(board);
+		return "redirect:/board/openBoardList.do";
+	}
+}
