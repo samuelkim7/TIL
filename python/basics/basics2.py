34921cb0f5aaba16c6ed05ff67718a6f20c400d2
Samuel <samuelmwkim@gmail.com>
2020-12-30 15:09:28 +0900

    python basic practice

---
+++ python/basics/basics2.py
@@ -0,0 +1,18 @@
+## format
+# print("나는 %d살이고 %s를 좋아합니다." % (15, "빨간색"))
+# print("나는 {}살입니다.".format(30))
+
+# age = 20
+# color = "빨간"
+# print(f"나는 {age}살이며, {color}색을 좋아해요.")
+
+# import random
+# total = 0
+# for i in range(1, 51):
+#     time = random.randint(5, 50)
+#     check = " "
+#     if time >= 5 and time <= 15:
+#         check = "O"
+#         total += 1
+#     print("[{}] {}번째 손님 (소요시간 : {}분)".format(check, i, time))
+# print("총 탑승 승객 : {} 분".format(total))
