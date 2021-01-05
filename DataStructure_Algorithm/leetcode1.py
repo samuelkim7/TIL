3d5a525f6716553e0fea2fe122eaf5361b9c5fb8
Samuel <samuelmwkim@gmail.com>
2021-01-05 15:25:17 +0900

    coding test practice

--- DataStructure_Algorithm/leetcode1.py
+++ DataStructure_Algorithm/leetcode1.py
@@ -129,3 +129,27 @@ class MyStack:
 
     def empty(self) -> bool:
         return len(self.q) == 0
+
+# Implement Queue using Stacks
+class MyQueue:
+    def __init__(self):
+        self.stack1, self.stack2 = [], []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        self.move()
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        self.move()
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        return (not self.stack2) and (not self.stack1)
+
+    def move(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
