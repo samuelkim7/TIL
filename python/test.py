a932584c070335b30961711edc276f42f7bd1f1d
Myeongwon Kim <65876994+samuelkim7@users.noreply.github.com>
2021-10-27 00:59:36 +0900

    Update test.py

--- python/test.py
+++ python/test.py
@@ -26,14 +26,13 @@ frame_cnt = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print('Number of frame: {} / FPS: {} / Frame size: {}' \
       .format(frame_cnt, round(vid_fps), vid_size))
 
-# defining dilation mask to apply dilation 
-dilation_mask = np.zeros((height, width), dtype='bool')
-border_area = 0.03
-border_height = round(height * border_area)
+# defining closing structure and dilation mask 
+closing_ref = np.ones((2,round(width * 0.3),1), dtype='bool')
 
+dilation_mask = np.zeros((height, width), dtype='bool')
+border_height = round(height * 0.03)
 dilation_mask[:border_height, :] = True
 dilation_mask[height-border_height:, :] = True
-
 dilation_mask = np.expand_dims(dilation_mask, axis=-1)
 
 # generating the output video with segmentation
@@ -44,7 +43,7 @@ target_classes = {
     'tie': 'invalid'
 }
 bg_img_resized = cv2.resize(bg_img, (width, height))
-closing_ref = np.ones((2,round(width * 0.3),1), dtype=int)
+
 
 index = 0
 while True:
@@ -53,15 +52,12 @@ while True:
     print('no more frame to be processed')
     break
   
-  # segment and change by deeplabv3
-  # output_deeplab = change_bg.change_frame_bg(img_frame, bg_path)
-  
-  # segment and change by pointrend 
+  # segment by PointRend 
   result = ins.segmentFrame(img_frame.copy(), segment_target_classes=target_classes)
   person_mask = result[0]['masks'][:, :, 0]
   person_mask = np.expand_dims(person_mask, axis=-1)
   
-  # applying closing and dilation
+  # closing and dilation
   person_mask_clos = ndimage.binary_closing(person_mask, 
                                            iterations=1,
                                            structure=closing_ref)
@@ -70,24 +66,7 @@ while True:
                                             iterations=2,
                                             mask=dilation_mask)
 
-  output_pr = np.where(person_mask, img_frame, bg_img_resized)
-  output_pr_cvt = cv2.cvtColor(output_pr, cv2.COLOR_BGR2RGB)
-  fig = plt.figure(figsize=(30, 20))
-  ax1 = fig.add_subplot(1, 3, 1)
-  ax1.imshow(output_pr_cvt)
-
-  output_pr_dil = np.where(person_mask_dil, img_frame, bg_img_resized)
-  output_pr_dil_cvt = cv2.cvtColor(output_pr_dil, cv2.COLOR_BGR2RGB)
-  ax1 = fig.add_subplot(1, 3, 2)
-  ax1.imshow(output_pr_dil_cvt)
-
-  # combine the two results
-  # final_output = np.where(person_mask, output_pr, output_deeplab)
-  # final_output = cv2.cvtColor(final_output, cv2.COLOR_BGR2RGB)
-  # ax2 = fig.add_subplot(1, 3, 3)
-  # ax2.imshow(final_output)
-  break
-
+  output_dil = np.where(person_mask_dil, img_frame, bg_img_resized)
   writer.write(output_pr)
 
   index += 1
